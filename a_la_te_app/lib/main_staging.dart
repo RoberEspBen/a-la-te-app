77c5e78243e978ae8901b4b74b7ca434f4b2f6ef
[RoberEspBen] <[rceb2@alu.ua.es]>
2023-02-04 18:57:31 +0100

    very-good-cli init proyect created

---
+++ a_la_te_app/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:a_la_te_app/app/app.dart';
+import 'package:a_la_te_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ a_la_te_app/test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:a_la_te_app/app/app.dart';
+import 'package:a_la_te_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
